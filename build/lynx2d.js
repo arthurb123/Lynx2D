5d39593c66c3354a1cf41e380fff2bda4cc0fa1a
Arthur <arthieb@live.nl>
2019-06-02 18:19:03 +0200

    New feature: classical perlin noise object

--- build/lynx2d.js
+++ build/lynx2d.js
@@ -2143,6 +2143,73 @@ this.GameObject = function (sprite, x, y, w, h) {
         console.log('GameObjectError: Created a GameObject without a specified size.');
 };
 
+/* Noise Object */
+
+this.Noise = function(seed) { 
+    if (seed == undefined) 
+        seed = Math.round(Math.random()*10000);
+
+    this.GRAD = [[1,1],[-1,1],[1,-1],[-1,-1], 
+                 [1,0],[-1,0],[1,0],[-1,0], 
+                 [0,1],[0,-1],[0,1],[0,-1]]; 
+
+    this.RANDOM = function() {
+        let x = Math.sin(seed++) * 10000;
+
+        return x - Math.floor(x);
+    };
+
+    this.P = [];
+    for (let i=0; i<256; i++) 
+        this.P[i] = Math.floor(this.RANDOM()*256);
+
+    this.PERM = []; 
+    for(var i=0; i<512; i++) 
+        this.PERM[i]=this.P[i & 255];
+
+    this.DOT = function(g, x, y) { 
+        return g[0]*x + g[1]*y; 
+    };
+
+    this.MIX = function(a, b, t) { 
+        return (1.0-t)*a + t*b; 
+    };
+
+    this.FADE = function(t) { 
+        return t*t*t*(t*(t*6.0-15.0)+10.0); 
+    };
+
+    this.Get = function(x, y) { 
+        let X = Math.floor(x),
+            Y = Math.floor(y); 
+  
+        x = x - X;
+        y = y - Y;
+  
+        X = X & 255; 
+        Y = Y & 255; 
+  
+        let gi00 = this.PERM[X+this.PERM[Y]] % 12,
+            gi01 = this.PERM[X+this.PERM[Y+1]] % 12,
+            gi10 = this.PERM[X+1+this.PERM[Y]] % 12,
+            gi11 = this.PERM[X+1+this.PERM[Y+1]] % 12; 
+
+        let n00 = this.DOT(this.GRAD[gi00], x, y),
+            n10 = this.DOT(this.GRAD[gi10], x-1, y),
+            n01 = this.DOT(this.GRAD[gi01], x, y-1),
+            n11 = this.DOT(this.GRAD[gi11], x-1, y-1);
+
+        let u = this.FADE(x),
+            v = this.FADE(y);
+ 
+        let nx0 = this.MIX(n00, n10, u),
+            nx1 = this.MIX(n01, n11, u), 
+            nxy = this.MIX(nx0, nx1, v);
+
+        return nxy; 
+    };
+};
+
 /* Ray Object */
 
 this.Ray = function(x, y) {
--- playground/playground.js
+++ playground/playground.js
@@ -1,24 +1,35 @@
-lx.GAME.SETTINGS.AA = false;
+lx.Smoothing(false);
+lx.ParticleLimit(3000);
+
 lx.GAME.DEBUG = true;
-lx.GAME.SETTINGS.LIMITERS.PARTICLES = 3000;
 
 //Lynx2D code goes here.
 
-let ball = new lx.Sprite('res/ball.png');
-let emitter = new lx.Emitter(ball, 0, 0, 32, 32);
+let emitter = new lx.Emitter(new lx.Sprite('res/ball.png'), 0, 0, 32, 32);
 
 new lx.Sprite('res/human.png', function(sprite) {
     let humanSprites = lx.CreateVerticalTileSheet(sprite, 48, 96);
     let human = new lx.GameObject(sprite, -24, -64, 48, 96);
     
     human.ShowAnimation(new lx.Animation(humanSprites[1], 12));
-    human.Show(1);
+    human.Show(2);
 });
 
-emitter.Show(0);
+emitter.Show(1);
 emitter.Speed(4);
 emitter.Setup(0, 0, -2, 12, 32, 32);
 emitter.MovementDecelerates(false);
 emitter.Range(128, 0);
 
 emitter.Follows(new lx.GameObject(undefined, 0, 0, 1, 1).Focus());
+
+let noise = new lx.Noise();
+
+for (let x = 0; x < 8; x++)
+    for (let y = 0; y < 8; y++) {
+        let r = Math.abs(noise.Get(x/5, y/5));
+
+        new lx.GameObject(new lx.Sprite('res/block.png'), (x-4)*32, (y-4)*32, 32, 32)
+        .Opacity(r)
+        .Show(0);
+    }
