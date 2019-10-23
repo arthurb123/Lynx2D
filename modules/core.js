6f8fc81a78bbd92f507e1905de9025b40978d98f
Arthur <arthieb@live.nl>
2019-10-23 19:42:21 +0200

    Removed automatic cleanup as it may interfer with some projects, rework is required

--- modules/core.js
+++ modules/core.js
@@ -286,12 +286,10 @@ this.GAME = {
         for (let i = 0; i < this.BUFFER.length; i++) {
             try {
                 if (this.BUFFER[i] != undefined) 
-                    for (let ii = 0; ii < this.BUFFER[i].length; ii++) {
-                        if (this.BUFFER[i][ii] != undefined) 
-                            this.BUFFER[i][ii].RENDER();
-                        else
-                            this.BUFFER[i].splice(ii, 1);
-                    }
+                    this.BUFFER[i].forEach(function(obj) {
+                        if (obj != undefined) 
+                            obj.RENDER();
+                    });
             } catch (err) {
                 lx.GAME.LOG.ERROR('BufferRenderError', err);
             };
